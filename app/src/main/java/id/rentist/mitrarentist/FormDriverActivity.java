f154adef19b8f95a2554042903f44ef338919484
ngPambudi <nugrohotripambudi@gmail.com>
2017-08-24 00:58:15 +0700

    swipe refresh pengguna dan driver

--- app/src/main/java/id/rentist/mitrarentist/FormDriverActivity.java
+++ app/src/main/java/id/rentist/mitrarentist/FormDriverActivity.java
@@ -4,13 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
-
-import id.rentist.mitrarentist.adapter.FormDriverAdapter;
 
 public class FormDriverActivity extends AppCompatActivity {
     RecyclerView.Adapter mAdapter;
@@ -27,12 +24,12 @@ public class FormDriverActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.edr_recyclerView);
-        mLayoutManager = new LinearLayoutManager(getApplicationContext());
-        mAdapter = new FormDriverAdapter();
+//        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.edr_recyclerView);
+//        mLayoutManager = new LinearLayoutManager(getApplicationContext());
+//        mAdapter = new FormDriverAdapter();
 
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
+//        mRecyclerView.setLayoutManager(mLayoutManager);
+//        mRecyclerView.setAdapter(mAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
